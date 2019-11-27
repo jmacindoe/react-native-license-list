3ec3e687a5669fdf42546a36bcda79e16b514f3b
James Macindoe <james.macindoe@gmail.com>
2019-11-27 15:04:15 +1100

    Use cli arguments

--- cli/src/index.ts
+++ cli/src/index.ts
@@ -1,15 +1,26 @@
 import { buildLicenseTree } from "./buildLicenseTree"
 import { npmLs } from "./npmLs"
 import { readLicenseData } from "./readLicenseData"
+import { parseArgs } from "./args"
+import fs from "fs"
 
 async function main() {
+  const args = parseArgs()
+
+  // npm ls will run in the current working directory, we can't tell it where to run
+  const cwd = process.cwd()
+  const start = args.project || "."
+  process.chdir(start)
+
   const lsTree = await npmLs()
   const licenseData = await readLicenseData({
     start: ".",
     relativeLicensePath: true,
   })
   const output = buildLicenseTree(lsTree, licenseData)
-  console.log(output)
+
+  process.chdir(cwd)
+  fs.writeFileSync(args.outputPath, JSON.stringify(output, null, 2))
 }
 
 main()
